dd52970041b6b2ff80fb63832d50e7c8e5de34b7
Gamzat Shakhmanaev <95529471+zhigibig@users.noreply.github.com>
2022-12-15 15:27:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,38 @@
 import pandas as pd
 from random import randint
+import generators as gen 
+# import matplotlib.pyplot as plt
+# import math
 
 
-
-def date_type(day, month, year):
-    return f"{day}:{month}:{year}"
-
-def times_of_day_type():
-    arr = ["day", "night"]
-    return arr[randint(0, 1)]
-
-def day_of_the_week_type():
-    days = ["week day", "day off"]
-    return days[randint(0, 1)]
-
-def time_type():
-    hour = randint(1, 23)
-    minutes = randint(0, 1)
-    if minutes:
-        minutes = 30
-        return f"{hour}:{minutes}"
-    else:
-        minutes = "00"
-        return f"{hour}:{minutes}"
-
-def film_type():
-    films = ["Брат", "Брат 2", "Груз-200", "Курьер", "Терминал",
-             "Одныжды в Голливуде", "Бесславные Ублюдки",
-             "Все ненавидят Йохана", "Сделано в Америе", "Золото",
-             "Трамбо", "Оно", "Оно 2", "Малыш на драйве", "Драйв",
-             "Стрелок", "Кингсман. Секретная миссия", "Телефонная будка",
-             "Шоу Трумана", "Лицо со шрамом", "Волк с Уолл-Стрит",
-             "Операция Колибри", "Паразиты"]
-    return films[randint(0, len(films) - 1)]
-
-def genre_type():
-    genres = ["Боевик", "Боевик", "Триллер", "Триллер",
-              "Драма", "Вестерн", "Боевик", "Трагикомедия",
-              "Боевик", "Боевик", "Приключения", "Ужас", "Ужас",
-              "Триллер", "Боевик", "Боевик", "Боевик", "Трагедия",
-              "Драма", "Боевик", "Комедия", "Детектив", "Трагедия"]
-    return genres[randint(0, len(genres) - 1)]
-
-def cinema_hall_type():
-    return f"{randint(0, 25)}"
-
-def row_type():
-    return randint(0, 10)
-
-def seat_type():
-    return randint(0, 15)
-
-def price_type():
-    arr = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000]
-    return arr[randint(0, len(arr) - 1)]
 
 def main():
-    count = 5000
-    df = pd.DataFrame({
-        "Film": [film_type() for i in range(count)],
-        "Genre": [genre_type() for i in range(count)],
-        "Date": [date_type(randint(1, 30), randint(1, 12), 2021) for i in range(count)],
-        "Day of the Week": [day_of_the_week_type() for i in range(count)],
-        "Time of the day": [times_of_day_type() for i in range(count)],
-        "Hall": [cinema_hall_type() for i in range(count)],
-        "Row": [row_type() for i in range(count)],
-        "Seat": [seat_type() for i in range(count)],
-        "Session time": [time_type() for i in range(count)],
-        "Price of ticket": [price_type() for i in range(count)],
-    })
+    count = randint(70_000, 140_000)
+
+    columns = ['First Name', 'Middle Name', 'Last Name', 'Date', "Session's time", "Day time",
+                # "Day of the week", 'Weekend', 
+                "Film", "Genre", 'Hall', "Row", "Seat", "Ticket's Price"]
+    table = {}
+    for i in columns:
+        table[f'{i}'] = []
+    
+    for i in range(count):
+        instance = [
+            gen.clients_fname(), gen.c_mname(), gen.c_lname(), 
+            gen.random_date(), gen.sessions_time(), gen.day_time(), 
+            #gen.day_of_the_week(),
+            # gen.doff_or_weekd(), 
+            gen.film(), gen.genre(),
+            gen.hall_num(), gen.row(), gen.seat(), gen.pricing()
+        ]
+
+        for e in range(len(columns)):
+            table[columns[e]].append(instance[e])
+    
+    df = pd.DataFrame(table)
 
     df.to_excel('./table.xlsx')
-    # df.to_sql('./') 
+    # df.to_sql('./table.sql') 
     
     return 0
 
