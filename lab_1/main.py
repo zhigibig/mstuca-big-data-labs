4cc27442dd2368c74b1ec132c773432a884426a9
Gamzat Shakhmanaev <95529471+zhigibig@users.noreply.github.com>
2022-12-29 21:34:25 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,13 +2,13 @@ import pandas as pd
 import data_generators as gen 
 from sqlalchemy import create_engine 
 from addresses_of_files import excel_table_adress as eta
-# import matplotlib.pyplot as plt
-# import math
+
+
 
 def data_exporting(data):
     data.to_excel(eta)
-    engine = create_engine('sqlite://', echo=False)
-    data.to_sql('./data.sql', con=engine)
+    # engine = create_engine('sqlite://', echo=False)
+    # data.to_sql('./data.sql', con=engine)
 
 def table_creation(number_of_lines):
     columns = [
@@ -31,11 +31,32 @@ def table_creation(number_of_lines):
 
         for e in range(len(columns)):
             table[columns[e]].append(instance[e])
+    
+    return table
 
 def main():
     NUMBER_OF_LINES = 137_822
 
-    table = table_creation(NUMBER_OF_LINES)
+    columns = [
+                'First Name', 'Middle Name', 'Last Name', 'Date', "Session's time", "Day time",
+                "Film", "Genre", 'Hall', "Row", "Seat", "Ticket's Price",
+              ]
+
+    table = {}
+
+    for i in columns:
+        table[f'{i}'] = []
+    
+    for i in range(NUMBER_OF_LINES):
+        instance = [
+            gen.clients_fname(), gen.c_mname(), gen.c_lname(), 
+            gen.random_date(), gen.sessions_time(), gen.day_time(), 
+            gen.film(), gen.genre(), gen.hall_num(), 
+            gen.row(), gen.seat(), gen.pricing(),
+        ]
+
+        for e in range(len(columns)):
+            table[columns[e]].append(instance[e])
     
     df = pd.DataFrame(table)
     
