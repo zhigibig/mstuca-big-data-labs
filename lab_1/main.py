38f1851a2f9fd4842dc41d2666ca87eeda0d15af
Gamzat Shakhmanaev <95529471+zhigibig@users.noreply.github.com>
2023-02-10 23:58:39 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,14 +1,29 @@
 import pandas as pd
 import data_generators as gen 
-from sqlalchemy import create_engine 
-from addresses_of_files import excel_table_adress as eta
-
-
+import sqlite3
+from addresses_of_files import excel_table_adress, sql_database_adress
+from sqlalchemy import create_engine
 
 def data_exporting(data):
-    data.to_excel(eta)
-    # engine = create_engine('sqlite://', echo=False)
-    # data.to_sql('./data.sql', con=engine)
+    data.to_excel(excel_table_adress)
+
+    conn = sqlite3.connect('Customer_base')
+    c = conn.cursor()
+
+    c.execute('CREATE TABLE IF NOT EXISTS products (fname text, sname text, lname text, date text, session_time text, day_time text, film text, genre text, hall number, row number, seat number, price number)')
+    conn.commit()
+
+    data.to_sql('Customer_base', conn, if_exists='replace', index = False)
+ 
+    c.execute('SELECT * FROM Customer_base')
+
+    for r in c.fetchall():
+        print (r)
+    
+    data.to_csv('Table in CSV type', sep='\t', encoding='utf-8')
+
+    return 0
+
 
 def table_creation(number_of_lines):
     columns = [
